a79c008932008bc1360c2255512a7eb31fdc52c1
Chase M Fensore <cmf2018@gmail.com>
2024-01-29 22:05:47 -0500

    Pharmacy data zip summary counts.

--- compress_pharma.py
+++ compress_pharma.py
@@ -1,13 +1,29 @@
 import json
 import pandas as pd
 
+"""
+This script writes a json file with zip codes (key) and the number of pharmacies in that zip code (value).
+Adjust code to filter for 'source': either 'gp' or 'fsq' 
+"""
 
-tmp = pd.read_csv("pharmacy_ga.csv")
+# Read df in
+tmp = pd.read_csv("../Data/GA_Pharmacy_Data_gp_fsq/pharmacy_ga.csv")
+
+# Drop rows where zipcode is None
+tmp = tmp.dropna(subset=['zipcode'])
+tmp = tmp.reset_index(drop=True)
+
+# Cast zipcode to int
 tmp.zipcode = tmp.zipcode.astype(int)
 
-tmp_gp = tmp[tmp.source == 'gp']
+# To select pharmacies from Google Places: 'gp'
+# To select pharmacies from Foursquare: 'fsq'
+tmp_gp = tmp[tmp.source == 'fsq']
+
+# Count number of pharmacies per zipcode
 agg_gp = tmp_gp.groupby(by=["zipcode"]).count()
 agg_json = agg_gp.to_dict()
 
-with open("zipdata.json", "w") as outfile:
+# Write to json -- 'census' column is arbitrary, used to write dict keys, values at 'zipcode' level.
+with open("fsq_zipdata.json", "w") as outfile:
     outfile.write(json.dumps(agg_json["census"]))
